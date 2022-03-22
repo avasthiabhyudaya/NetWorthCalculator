31c71f51bd22fda2372122dd337f2825b8e3dd69
Abhyudaya Avasthi <abhyudayaavasthi@Abhyudayas-MacBook-Air.local>
2022-03-22 22:09:14 +0530

    changing BrowserRouter to HashRouter

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
-import { BrowserRouter } from "react-router-dom"
+import { HashRouter } from "react-router-dom"
 import { PersistGate } from 'redux-persist/es/integration/react';
 import { Provider } from 'react-redux'
 import { store, persistor } from './store'
@@ -12,9 +12,9 @@ ReactDOM.render(
 
   <Provider store={store}>
     <PersistGate persistor={persistor}>
-      <BrowserRouter>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </PersistGate>
   </Provider>,
   document.getElementById('root')
